0edac2888b49a6b6026167feed9a633a8ad98472
Abiu Roldan <abiuroldan@2806-10ae-0004-d185-1c6d-e4d6-34e0-e38d.ipv6.infinitum.net.mx>
2017-11-08 19:09:59 -0600

    Add verify on email address and password, with error message

--- Zapatos/Controller/LoginViewController.swift
+++ Zapatos/Controller/LoginViewController.swift
@@ -29,19 +29,30 @@ class LoginViewController: UIViewController {
     
     
     @IBAction func logInButtom(_ sender: Any) {
-        SVProgressHUD.show()
-        
-        //TODO: Log In the user
-        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+        if emailTextField.text == "" || passwordTextField.text == ""{
+            SVProgressHUD.showError(withStatus: "Please insert Email or Password")
+            let d_one = TimeInterval(1)
+            SVProgressHUD.dismiss(withDelay: d_one)
+        }
+        else{
+            SVProgressHUD.show()
             
-            if error != nil{
-                print(error)
-            }
-            else
-            {
-                print("Logging Successful")
-                SVProgressHUD.dismiss()
-                self.performSegue(withIdentifier: "goToShoes", sender: self)
+            //TODO: Log In the user
+            Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+                
+                if error != nil{
+                    print(error)
+                    SVProgressHUD.showError(withStatus: "Please verify Email or Password")
+                    let timeD = TimeInterval(1)
+                    SVProgressHUD.dismiss(withDelay: timeD)
+                }
+                else
+                {
+                    print("Logging Successful")
+                    SVProgressHUD.dismiss()
+                    self.performSegue(withIdentifier: "goToShoes", sender: self)
+                }
+            
             }
         }
     }
